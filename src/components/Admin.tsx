3c40a540e24d0eb6051f0a26a044729192cce9b2
Stian Fiko <stian.fiko@gmail.com>
2025-08-22 13:26:47 +0000

    Nå bare laster den data igjen

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import type { Student, Subject } from "@/lib/types";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
