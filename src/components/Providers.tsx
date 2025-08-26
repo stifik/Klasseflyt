509a08fe1806848ab1a785d31dbec5c636df4725
Stian Fiko <stian.fiko@gmail.com>
2025-08-26 12:01:56 +0000

    Nå er siden publisert på nytt, men ved første besøk har den mørkt tema,

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -6,7 +6,7 @@ import { msalInstance } from "@/auth/msal";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
             <MsalProvider instance={msalInstance}>
                 {children}
             </MsalProvider>
