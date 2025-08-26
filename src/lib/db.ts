d7255a13ee59855ce5d77c9de654ab69e8a41f89
Stian Fiko <stian.fiko@gmail.com>
2025-08-26 19:31:26 +0000

    [project]/src/lib/db.ts [app-client] (ecmascript)/resetDatabase/<@https:

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -151,24 +151,23 @@ export async function resetDatabase() {
         const studentIds = await db.students.bulkAdd(mockStudents, { returning: true }) as string[];
         const subjectIds = await db.subjects.bulkAdd(mockSubjects, { returning: true }) as string[];
         
-        // Re-fetch subjects to get full objects
-        const subjects = await db.subjects.bulkGet(subjectIds);
+        // Re-fetch subjects to get full objects with IDs
+        const subjects = await db.subjects.bulkGet(subjectIds) as Subject[];
 
-        const norskSubject = subjects.find(s => s?.name === 'Norsk');
-        const engelskSubject = subjects.find(s => s?.name === 'Engelsk');
-        const matteSubject = subjects.find(s => s?.name === 'Matematikk');
-        const naturfagSubject = subjects.find(s => s?.name === 'Naturfag');
+        const norskSubject = subjects.find(s => s && s.name === 'Norsk');
+        const engelskSubject = subjects.find(s => s && s.name === 'Engelsk');
+        const matteSubject = subjects.find(s => s && s.name === 'Matematikk');
+        const naturfagSubject = subjects.find(s => s && s.name === 'Naturfag');
 
         // --- Create Mock Homework ---
         const today = new Date();
         const thisWeek = getWeekNumber(today);
         const homeworkToAdd: Omit<Homework, 'id'>[] = [];
 
-        if (norskSubject) homeworkToAdd.push({ title: "Lesing kap. 2", subjectId: norskSubject.id!, week: thisWeek, date: new Date() });
-        if (engelskSubject) homeworkToAdd.push({ title: "Gloser", subjectId: engelskSubject.id!, week: thisWeek, date: new Date() });
-        if (matteSubject) homeworkToAdd.push({ title: "Oppg. 3.1-3.5", subjectId: matteSubject.id!, week: thisWeek, date: new Date() });
-        if (naturfagSubject) homeworkToAdd.push({ title: "Verdensrommet", subjectId: naturfagSubject.id!, week: thisWeek - 1, date: new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000) });
-
+        if (norskSubject?.id) homeworkToAdd.push({ title: "Lesing kap. 2", subjectId: norskSubject.id, week: thisWeek, date: new Date() });
+        if (engelskSubject?.id) homeworkToAdd.push({ title: "Gloser", subjectId: engelskSubject.id, week: thisWeek, date: new Date() });
+        if (matteSubject?.id) homeworkToAdd.push({ title: "Oppg. 3.1-3.5", subjectId: matteSubject.id, week: thisWeek, date: new Date() });
+        if (naturfagSubject?.id) homeworkToAdd.push({ title: "Verdensrommet", subjectId: naturfagSubject.id, week: thisWeek - 1, date: new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000) });
         
         const homeworkIds = await db.homework.bulkAdd(homeworkToAdd, { returning: true }) as number[];
 
