62430c5a8a527ffdce64b6255cb48edad4fe8d73
Stian Fiko <stian.fiko@gmail.com>
2025-09-24 13:37:19 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -4,6 +4,10 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function AppThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
+      {children}
+    </NextThemesProvider>
+  )
 }
