342bc3872f58a92cb0373ece6f10f6b3b2f63afe
Stian Fiko <stian.fiko@gmail.com>
2025-09-25 06:18:20 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import { AppThemeProvider } from '@/components/ThemeProvider';
+import { Providers } from '@/components/Providers';
 
 export const metadata: Metadata = {
   title: 'Klasseflyt',
@@ -21,10 +21,10 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <AppThemeProvider>
+        <Providers>
           {children}
           <Toaster />
-        </AppThemeProvider>
+        </Providers>
       </body>
     </html>
   );
